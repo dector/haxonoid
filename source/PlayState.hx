4d0b8867e80ac9e63a387338380e3b6d6a193e65
Denys M <dector9@gmail.com>
2014-07-02 23:00:03 +0300

    Great walls of fire

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxGroup;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -10,12 +11,14 @@ import flixel.util.FlxMath;
 class PlayState extends FlxState
 {
 	private var bat: FlxSprite;
+	private var walls: FlxGroup;
 
 	override public function create():Void
 	{
 		super.create();
 
 		createBat();
+		createWalls();
 	}
 
 	private function createBat(): Void
@@ -23,6 +26,24 @@ class PlayState extends FlxState
 		bat = new FlxSprite(270, 420);
 		bat.makeGraphic(100, 20, 0xffffffff);
 		add(bat);
+	}
+
+	private function createWalls(): Void
+	{
+		walls = new FlxGroup();
+		add(walls);
+
+		var left = new FlxSprite(0, 0);
+		left.makeGraphic(20, 480, 0xff999999);
+		walls.add(left);
+
+		var right = new FlxSprite(620, 0);
+		right.makeGraphic(20, 480, 0xff999999);
+		walls.add(right);
+
+		var top = new FlxSprite(0, 0);
+		top.makeGraphic(640, 20, 0xff999999);
+		walls.add(top);
 	}
 	
 	override public function destroy():Void
@@ -40,11 +61,11 @@ class PlayState extends FlxState
 
 	private function moveBat(): Void
 	{
-		if (FlxG.keys.pressed.LEFT && bat.x > 0)
+		if (FlxG.keys.pressed.LEFT && bat.x > 20)
 		{
 			bat.velocity.x = -500;
 		}
-		else if (FlxG.keys.pressed.RIGHT && bat.x < 540)
+		else if (FlxG.keys.pressed.RIGHT && bat.x < 520)
 		{
 			bat.velocity.x = 500;
 		}
